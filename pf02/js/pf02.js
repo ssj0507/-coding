82cb2faf24c54e4401106fd485621df3a71d0e6a
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-11-09 17:52:14 +0900

    Add files via upload

--- pf02/js/pf02.js
+++ pf02/js/pf02.js
@@ -12,14 +12,28 @@ $(function(){
     });
 
     $('.main_slider').slick({
-       // autoplay:true,
+        //autoplay:true,
         autoplaySpeed:800,
         pauseOnHover:false,
         arrows:false,
         dots:true,
     });
 
+    //------화살표------//
 
+    $('#main_visual .main_btn i.xi-angle-left-thin').on('click', function(){
+        $('.main_slider').slick('slickPrev');
+    });
+    $('#main_visual .main_btn i.xi-angle-right-thin').on('click', function(){
+        $('.main_slider').slick('slickNext');
+    });
 
+    //------글자효과------//
+
+    $('.main_slider figure').eq(1).addClass('on')
+    $('.main_slider').on('afterChange',function(e,s,c){
+        //console.log(c);
+        $('.main_slider figure').eq(c+1).addClass('on').siblings().removeClass('on');
+    });
 
 });  
