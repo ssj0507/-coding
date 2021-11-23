a0df65700ce18e3532ffc04d0e58ea84f81df4b0
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-11-23 15:20:58 +0900

    Add files via upload

--- pf03/js/pf03.js
+++ pf03/js/pf03.js
@@ -13,7 +13,7 @@ $(function(){
 
     $('.main_slider').slick({
         autoplay:true,
-        autoplaySpeed:3000,
+        autoplaySpeed:4000,
         pauseOnHover:false,
         arrows:false,
         dots:true,
