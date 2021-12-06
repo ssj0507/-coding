8be1f735d756b8679fbd8c62dbf1ff00e2cf28f3
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-12-06 16:17:14 +0900

    Add files via upload

--- pf04/js/pf04.js
+++ pf04/js/pf04.js
@@ -1,109 +1,146 @@
-$(function(){
-    // 스크롤할때 헤드변경
+$(function () {
+  // 스크롤할때 헤드변경
 
-    $(window).on('scroll', function(){
-        var sct=$(window).scrollTop();
-        console.log(sct);
-        sct > 0 ? $('.herder').addClass('on') : $('.herder').removeClass('on');
-    });
+  $(window).on("scroll", function () {
+    var sct = $(window).scrollTop();
+    console.log(sct);
+    sct > 0 ? $(".herder").addClass("on") : $(".herder").removeClass("on");
+  });
 
-    $('.main_slider').slick({
-        autoplay:true,
-        pauseOnHover:false,
-        pauseOnFocus:false,
-        fade:true,
-        arrows:false,
-        dots:true,
-    });
+ //메인 슬라이드
+  $(".main_slider").slick({
+    autoplay: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    fade: true,
+    arrows: false,
+    dots: true,
+  });
+
+ //------메인 화살표------//
+
+  $(".main_visual .main_btn i.xi-angle-left-thin").on("click", function () {
+    $(".main_slider").slick("slickPrev");
+  });
+  $(".main_visual .main_btn i.xi-angle-right-thin").on("click", function () {
+    $(".main_slider").slick("slickNext");
+  });
+
+//----------------------------------------------------------------//
+
+ //첫번째 js로 하는 slick 버전 -모바일//
+
+  var Rsd = $('.c_slider');
+
+  var slickOptions = {
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay:true,
+      autoplaySpeed:2000,
+      centerMode:true,
+      dots:false,
+      arrows:false,
+      
+  };
+
+  $(window).on('load resize', function() {
+      if($(window).width() > 768) {
+          Rsd.slick('unslick');
+      }else{
+          Rsd.not('.slick-initialized').slick(slickOptions);
+      }
+  });
+//---------------------------------------------------------------------------//
+
+  // 두번째 섹션 제품 슬라이드
+  $('.sc_slider').slick({
+    arrows:false,
+    asNavFor:'.sc_slider',
+    pauseOnHover:false,
+    pauseOnFocus:false,
+    autoplay:true,
+    autoplaySpeed:3500,
+
+
+  });
 
  //------화살표------//
 
- $('.main_visual .main_btn i.xi-angle-left-thin').on('click', function(){
-    $('.main_slider').slick('slickPrev');
-});
-$('.main_visual .main_btn i.xi-angle-right-thin').on('click', function(){
-    $('.main_slider').slick('slickNext');
-});
+  $(".content02 .con02_btn i.xi-angle-left-thin").on("click", function () {
+    $(".sc_slider").slick("slickPrev");
+  });
+  $(".content02 .con02_btn i.xi-angle-right-thin").on("click", function () {
+    $(".sc_slider").slick("slickNext");
+  });
 
-
-
-   
-
-//탭 메뉴//
-$('.tab_menu li').on('click', function(){
-    var idx=$(this).index();
-    // $('.tab_con>div').eq(idx).show().siblings().hide();
-    $('.con04_tab>div').eq(idx).addClass('on').siblings().removeClass('on');
-    $(this).addClass('on').siblings().removeClass('on');
-});
-
-//세번째 js로 하는 slick 버전//
-
-var Rsd = $('.r_slider');
-
-var slickOptions = {
-    infinite: true,
+//-----------------------------------------------------------//
+  // 네번째 섹션 제품 슬라이드
+  $(".con04_slider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed:2500,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay:true,
-    autoplaySpeed:2000,
-    centerMode:true,
-    dots:false,
-    arrows:false
-};
+  });
 
-$(window).on('load resize', function() {
-    if($(window).width() > 768) {
-        Rsd.slick('unslick');
-    }else{
-        Rsd.not('.slick-initialized').slick(slickOptions);
-    }
-});
+  // 네번째 섹션 제품 슬라이드 -선 움직임과 글자
+
+  $(".con04_menu li").eq(0).addClass("on");
+  $(".con04_slider").on('afterChange', function(e,s,c){
+      if($(window).width()>768) {
+        $('.line').css({top:50*c +275});
+      }else{
+        $('.line').css({left:50*c +40});
+      }
+      
+      $(".con04_menu li").eq(c).addClass("on").siblings().removeClass("on");
+
+  })
+
+  $(".con04_menu li").on('click', function(){
+      var idx=$(this).index();
+      $(".con04_slider").slick('slickGoTo', idx);
+      $(this).addClass("on").siblings().removeClass("on");
+  })
 
 
 
 
- 
 
 //toTop//
 
-$('.toTop').on('click', function(){
-    $('html, body').animate({scrollTop:0},400)
-});
+  $(".toTop").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 400);
+  });
 
-$(window).on('scroll', function(){
-    var sct=$(window).scrollTop();
+  $(window).on("scroll", function () {
+    var sct = $(window).scrollTop();
     //if(sct>400){
     //    $('#toTop').fadeIn()
-   // }else
+    // }else
     //$('#toTop').fadeOut()
     //}
-   sct>400 ? $('#toTop').fadeIn() : $('#toTop').fadeOut();
-   $(this).css({top:300+sct})
-});
+    sct > 400 ? $("#toTop").fadeIn() : $("#toTop").fadeOut();
+    $(this).css({ top: 300 + sct });
+  });
 
-//다단메뉴에서 호버부분을 ( #header nav>ul>li:hover .total {display:none;})을 할때 사용하는것이다.//
-$('#header nav>ul>li>a').on('click', function(e){
-    if($('.ddgf').hasClass('on')) {
-        e.preventDefault();
-        $(this).next().stop().slideToggle();
-        $(this).parent().siblings().find('.total').stop().slideUp();
+  //다단메뉴에서 호버부분을 ( #header nav>ul>li:hover .total {display:none;})을 할때 사용하는것이다.//
+  $("#header nav>ul>li>a").on("click", function (e) {
+    if ($(".ddgf").hasClass("on")) {
+      e.preventDefault();
+      $(this).next().stop().slideToggle();
+      $(this).parent().siblings().find(".total").stop().slideUp();
     }
+  });
+
+  //모바일용 메뉴//
+
+  $(".mopen").on("click", function () {
+    $(".gnb").toggleClass("on");
+    $(".search").fadeToggle();
+  });
+
+  $(window).on("resize", function () {
+    $(".total").removeAttr("style");
+  });
 });
-
-//모바일용 메뉴//
-
-$('.mopen').on('click', function(){
-    $('.gnb').toggleClass('on');
-    $('.search').fadeToggle();
-})
-
-$(window) .on('resize', function(){
-    $('.total').removeAttr('style')
-});
-
-
-
-
-
-});  
